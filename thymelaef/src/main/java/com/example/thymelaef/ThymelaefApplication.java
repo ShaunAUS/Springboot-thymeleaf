edbb31c16f139823645205bbf8e4368d9bec8f1e
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2021-07-15 02:17:46 +0900

    thymeleaf
    반복문, 기본사용법

---
+++ thymelaef/src/main/java/com/example/thymelaef/ThymelaefApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymelaef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymelaefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymelaefApplication.class, args);
+	}
+
+}
